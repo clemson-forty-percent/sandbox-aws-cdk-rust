3aea68224c97708102b400d663cd5b6995541e96
David Houston <houstdav000@gmail.com>
2021-02-08 18:00:03 -0500

    Add infra for lambda function

--- cdk/lib/sandbox-aws-cdk-rust-stack.ts
+++ cdk/lib/sandbox-aws-cdk-rust-stack.ts
@@ -1,9 +1,30 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigatewayv2';
+import * as apigwi from '@aws-cdk/aws-apigatewayv2-integrations';
 
 export class SandboxAwsCdkRustStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define lambda functions
+    const rustTest = new lambda.Function(this, 'rustTest', {
+        runtime: lambda.Runtime.PROVIDED_AL2,
+        code: lambda.Code.fromAsset('../lambda/hello/target/lambda/release/hello.zip'),
+        handler: 'doesnt.matter',
+    });
+
+    const rustTestIntegration = new apigwi.LambdaProxyIntegration({
+        handler: rustTest,
+    });
+
+    // Define API and routes
+    const httpApi = new apigw.HttpApi(this, 'HttpApi');
+
+    httpApi.addRoutes({
+        path: '/hello',
+        methods: [ apigw.HttpMethod.GET ],
+        integration: rustTestIntegration,
+    });
   }
 }
